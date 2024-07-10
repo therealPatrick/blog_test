4568d331273df9bc5560fd98c2f808e429b37d51
therealPatrick <malamashangala13@gmail.com>
2024-07-10 16:40:06 +0200

    Authentication with Devise

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem "sprockets-rails"
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", "~> 1.4"
 
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
 
@@ -65,3 +66,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "devise", "~> 4.9"
--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,4 +1,5 @@
 class BlogPostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_blog_post, except: [:index, :new, :create]
   def index
     @blog_posts = BlogPost.all
@@ -47,5 +48,9 @@ class BlogPostsController < ApplicationController
     @blog_post = BlogPost.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
+  end
+
+  def authenticate_user!
+    redirect_to new_user_session_path, alert: "You must sign in to continue." unless user_signed_in?
   end
 end
